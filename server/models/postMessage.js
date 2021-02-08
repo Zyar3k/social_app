303598f615d13a2844f27be4cc2de32e0017fd96
Arkadiusz Zygan <a.zygan@gmail.com>
2021-02-08 16:56:03 +0100

    Add schema

---
+++ server/models/postMessage.js
@@ -0,0 +1,13 @@
+const mongoose = require("mongoose");
+
+const postSchema = mongoose.Schema({
+  title: { type: String, required: true },
+  message: { type: String, required: true },
+  creator: { type: String, required: true },
+  tags: [String],
+  selectedFile: { type: String, required: true },
+  likeCount: { type: Number, default: 0 },
+  createdAt: { type: Date, default: new Date() },
+});
+
+module.exports = mongoose.model("Post", postSchema);
--- server/routes/posts.js
+++ server/routes/posts.js
@@ -1,9 +1,9 @@
 const express = require("express");
 
+const getPosts = require("../controllers/posts.js");
+
 const router = express.Router();
 
-router.get("/", (req, res) => {
-  res.send("this works");
-});
+router.get("/", getPosts);
 
 module.exports = router;
