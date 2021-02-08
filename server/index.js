c2f0b4452363a4f732e2363adaf12c14abba36d4
Arkadiusz Zygan <a.zygan@gmail.com>
2021-02-08 16:40:33 +0100

    Add routes

--- server/index.js
+++ server/index.js
@@ -4,7 +4,11 @@ const mongoose = require("mongoose");
 const cors = require("cors");
 require("dotenv/config");
 
+const postRoutes = require("./routes/posts");
+
 const app = express();
+
+app.use("/posts", postRoutes);
 
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
