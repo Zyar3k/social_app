f48fa4e23fbdfa46c7f90e17691ddcb844e4a60a
Arkadiusz Zygan <a.zygan@gmail.com>
2021-02-09 19:50:49 +0100

    Add posts & post component

--- client/src/components/Posts/Posts.js
+++ client/src/components/Posts/Posts.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { useSelector } from "react-redux";
 
-import { Grid } from "@material-ui/core";
+import { Grid, CircularProgress } from "@material-ui/core";
 
 import Post from "./Post/Post";
 import useStyles from "./styles";
@@ -12,30 +12,21 @@ const Posts = () => {
 
   console.log(posts);
 
-  return (
-    <>
-      <h1>Posts</h1>
-      <Grid
-        className={classes.container}
-        container
-        alignItems='stretch'
-        spacing={3}
-      >
-        <Grid item xs={12} sm={6} md={6}>
-          <Post />
+  return !posts.length ? (
+    <CircularProgress />
+  ) : (
+    <Grid
+      className={classes.container}
+      container
+      alignItems='stretch'
+      spacing={3}
+    >
+      {posts.map((post) => (
+        <Grid key={post._id} item xs={12} sm={6} md={6}>
+          <Post post={post} />
         </Grid>
-      </Grid>
-      <Grid
-        className={classes.container}
-        container
-        alignItems='stretch'
-        spacing={3}
-      >
-        <Grid item xs={12} sm={6} md={6}>
-          <Post />
-        </Grid>
-      </Grid>
-    </>
+      ))}
+    </Grid>
   );
 };
 
