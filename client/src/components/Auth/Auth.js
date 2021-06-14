5cb8e99e76ced7f87fe3e007e2215bba18fc8409
Arkadiusz Zygan <a.zygan@gmail.com>
2021-06-14 15:38:38 +0200

    Edit front layaout

---
+++ client/src/components/Auth/Auth.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+import useStyles from "./styles.js";
+
+const Auth = () => {
+  const state = null;
+  const classes = useStyles();
+
+  return (
+    <div>
+      <h1>auth</h1>
+    </div>
+  );
+};
+
+export default Auth;
