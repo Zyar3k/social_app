9528fcf547f666de3c4d6e8c5e4b32d64b540425
Arkadiusz Zygan <a.zygan@gmail.com>
2021-06-14 15:01:51 +0200

    Add new dependecies

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,5 +1,6 @@
 import axios from "axios";
 
+// const url = "https://social-mern-social.herokuapp.com/posts";
 const url = "https://social-mern-social.herokuapp.com/posts";
 
 export const fetchPosts = () => axios.get(url);
